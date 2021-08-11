e5d514391c6533ef1ad1c43ccf882020b5b30c55
natalyaKh <khrebtiivski@gmail.com>
2021-08-11 23:05:37 +0300

    add css check some bugs

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -22,18 +22,13 @@ $(function () {
 
   function addDataToTable() {
     $('#placeholder').hide();
-    // var tz = $('#tz').val()
-    // var name = $('#name').val()
-    // var secondName = $('#secondName').val()
-    // var startWork = $('#startWork').val()
-    // var birthday = $('#birthday').val()
 
     let data = {
       "tz": $('#tz').val(),
       "name": $('#name').val(),
       "lastName": $('#secondName').val(),
-      "birthday": $('#startWork').val(),
-      "startWork": $('#birthday').val()
+      "birthday": $('#birthday').val(),
+      "startWork": $('#startWor').val()
     };
     $.ajax({
       url: '/employee',
@@ -54,29 +49,6 @@ $(function () {
     $('#dialog').dialog("close");
   }
 
-  // function sendData() {
-  //   console.log(this.tz + " senddata")
-  // //   $.ajax({
-  //       url: '/employee',
-  //       type: 'POST',
-  //       async: false,
-  //       data: JSON.stringify(data)
-  //       "tz": this.tz,
-  //       "name": this.name,
-  //       "lastName": this.lastName,
-  //       "birthday": this.birthday,
-  //       "startWork": this.startWork
-  //     },
-  //     success
-  // :
-
-  //   function (resp) {
-  //     console.log(resp + " added")
-  //   }
-  // }
-
-// )
-// }
 
 function clearDialog() {
   $('#tz').val('')
@@ -85,35 +57,8 @@ function clearDialog() {
   $('#startWork').val('')
   $('#birthday').val('')
 }
-})
-;
+});
 
-
-// $('#changeButton').click(function() {
-//
-//   // console.log
-//   var form = $(this),
-//     name = form.find('input[name:"name"]')
-//   // var name = $('.exampleModalCenter').find('inpit[name="name"]').val();
-//   console.log(name + " k")
-//   // goods = $(this).data('goods');
-//   // $("#goods").append(goods);
-//
-// });
-// $(function() {
-//   $(".btn").click(
-//     function() {
-//       var imgtovara = $(this).attr('exampleFormControlInput2');
-//       // var nametitle = $(this).attr('data-nametitle');
-//       // var pricetovar = $(this).attr('data-pricetovar');
-// console.log(imgtovara);
-//       // $(".tovarimg").append('<img class="img-fluid" src="' + imgtovara + '" alt="..." />');
-//       // $(".tovarinfo").append('<p class="h3">' + nametitle + '</h1>');
-//       // $(".tovarinfo").append('<p><strong>Цена</strong>:' + pricetovar + '</p>');
-//       // $("#hide1").attr('value', nametitle);
-//       // $("#hide2").attr('value', pricetovar);
-//     })
-// });
 $(document).ready(function () {
   var table = $('#employeesTable').DataTable({
     searching: false,
@@ -126,15 +71,15 @@ $(document).ready(function () {
     "columnDefs": [{
       "targets": -1,
       "data": null,
-      "defaultContent": "<button class='btn btn-danger'>DELETE</button>",
+      "defaultContent": "<button class='delete-button'>DELETE</button>",
 
     }],
     "aoColumns": [
       {"mData": "tz"},
       {"mData": "name"},
       {"mData": "lastName"},
-      {"mData": "birthday"},
       {"mData": "workExperience"},
+      {"mData": "birthday"},
       {"mData": "action"}
     ]
   })
@@ -162,4 +107,22 @@ function deleteEmployee(id) {
 }
   })
 }
+
+$.ajax({
+  url: 'https://jsonip.com/',
+  type: 'GET',
+  success: function (resp) {
+    var outputText = 'ip: ' + resp.ip;
+    console.log(resp.ip + " ip")
+    $('#ip').append(outputText);
+  }
+})
+
+$.ajax({
+  type: "OPTIONS", url: "/", complete: function (x) {
+    console.log(x.getResponseHeader("Date"));
+  }
+})
+
+
 
